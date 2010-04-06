986167e0c1a8bb67ead8b6ba5301b8820b207a98
Amir Manji <amir.manji@cardplayer.com>
2010-04-06 09:49:26 -0700

    remove feeds, keep feed items

--- lib/abstract_feed_item.rb
+++ lib/abstract_feed_item.rb
@@ -5,19 +5,6 @@ class AbstractFeedItem < ActiveRecord::Base
   
   belongs_to :item, :polymorphic => true
   belongs_to :user
-  belongs_to :user_target, :class_name => 'User'
-  has_many :feeds, :dependent => :destroy
   
   validates_presence_of :item, :user
-  
-  named_scope :for_user, lambda { |user_id| { :conditions => [ "user_id = ? OR user_target_id = ?", user_id, user_id ], :order => 'created_at desc' } }
-  
-  after_create :create_feed
-  
-  def create_feed
-    Feed.create(:user => user, :feed_item => self)
-    Feed.create(:user => user_target, :feed_item => self) if (user_target && (user_target != user))
-    Delayed::Job.enqueue(GenerateFeedsJob.new(self.id))
-  end
-  
 end
