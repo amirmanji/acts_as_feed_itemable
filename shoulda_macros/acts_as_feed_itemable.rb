a95a943ae4c61a90d20e2f731763632c6a78d22d
Amir Manji <amir.manji@cardplayer.com>
2010-04-08 00:22:54 -0700

    clean up shoulda macro

--- shoulda_macros/acts_as_feed_itemable.rb
+++ shoulda_macros/acts_as_feed_itemable.rb
@@ -4,21 +4,21 @@ module ActsAsFeedItemable
     def should_act_as_feed_itemable(options = {})
       klass = self.name.gsub(/Test$/, '').constantize
       
-      context "Class #{klass.name} acting as a feed itemable" do
+      context "#{klass.name} acting as a feed itemable" do
         should_have_one :feed_item
         
         should "respond to all the right methods" do
           [ "process_feed_item", "feed_item_eligible?" ].each do |method|
-            assert klass.instance_methods.include?(method), "#{klass.name} does not respond to #{name}."
+            assert klass.instance_methods.include?(method), "#{klass.name} does not respond to #{method}."
           end
         end
       end
       
       if options[:with_flag]
         flag = options[:with_flag]
-        context "Class #{klass.name} acting as a feed itemable with flag #{flag}" do
+        context "#{klass.name} acting as a feed itemable with flag '#{flag}'" do
           [ "#{flag}", "#{flag_was}" ].each do |method|
-            assert klass.instance_methods.include?(method), "#{klass.name} does not respond to #{name}."
+            assert klass.instance_methods.include?(method), "#{klass.name} does not respond to #{method}."
           end
         end
       end
